a3c0d8d4a70814ca5b82ed5e8d7e625e4efb1981
Tim Landowski <timowland@gmail.com>
2014-10-23 12:03:22 -0700

    in class work

--- js/app.js
+++ js/app.js
@@ -4,3 +4,38 @@
 
 "use strict";
 
+$(function(){
+	var nav = $('nav');
+	var navTop = nav.offset().top;
+	var navHeight = nav.outerHeight();
+	var jqWin = $(window);
+	var navPlaceholder = $('.nav-placeholder');
+	navPlaceholder.height(navHeight);
+
+	jqWin.scroll(function(){
+		var scrollPos = $(this).scrollTop();
+		//console.log(scrollPos);
+		if (scrollPos > navTop) {
+			nav.addClass('nav-fixed');
+			navPlaceholder.show();
+		}
+		else {
+			nav.removeClass('nav-fixed');
+			navPlaceholder.hide();
+		}
+	});
+
+	$('section a[class!="back-to-top"]').attr('target', '_blank');
+
+	$('section').hide().fadeIn(1000);
+
+	$('nav a, a[class="back-to-top"]').click(function(eventObject) {
+		console.log(this.hash);
+		var targetElement = $(this.hash);
+		var targetTop = targetElement.offset().top;
+
+		$('html,body').animate({scrollTop: targetTop - navHeight - 10}, 700);
+
+		eventObject.preventDefault();
+	});
+});
